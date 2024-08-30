2faaf4b5723fabdc3b46731dc0334509a89b3cf1
MihailoRadulovic <161826642+MihailoRadulovic@users.noreply.github.com>
2024-08-30 19:42:42 +0200

    Add files via upload
    Zaminili smo vec postojeci CalenadrApp.jsx sa novim izmenjenim

--- src/Components/CalendarApp.jsx
+++ src/Components/CalendarApp.jsx
@@ -10,12 +10,18 @@ import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import User from "../Models/User";
 import Event from "../Models/Event";
-import React from 'react';
-
+import React from "react";
+import LoginPage from "./LogInPage";
+import Navbar from "./Navbar";
+import CreateEventComponent from "./CreateEventComponent";
+import CreateUserComponent from "./CreateUserComponent";
+import EventListComponent from "./EventListComponent";
+import UserListComponent from "./UserListComponent";
+import ProfilPage from "./ProfilPage";
+import RegisterPage from "./RegisterPage";
+import SuggestionPage from "./SuggestionPage";
 
 const CalendarApp = () => {
-
-
   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const monthsOfYear = [
     "January",
@@ -32,7 +38,6 @@ const CalendarApp = () => {
     "December",
   ];
 
-
   // Ovde su navedeni hook-voi koje koristimo u projektu
   const currentDate = new Date();
   const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
@@ -44,9 +49,6 @@ const CalendarApp = () => {
   const [eventText, setEventText] = useState("");
   const [editingEvent, setEditingEvent] = useState(null);
 
- 
-  
-  
   // Broj dana u mesecu / dan u sedmici za prvi dan meseca
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
@@ -90,7 +92,7 @@ const CalendarApp = () => {
     );
   };
 
-  // Ova funkcija sluzi za proveur podataka koje korisnik zeli da azurira ili doda u event list-u
+  // Ova funkcija sluzi za proveru podataka koje korisnik zeli da azurira ili doda u event list-u
   const handleEventSubmit = () => {
     // Kreiramo novi dogadjaj
     const newEvent = {
@@ -101,7 +103,6 @@ const CalendarApp = () => {
         "0"
       )}`,
       text: eventText,
-
     };
 
     // Kopiramo postojeci niz eventu u upadatedEvents zbog azuriranja
@@ -295,450 +296,6 @@ const CalendarApp = () => {
   );
 };
 
-const Navbar = () => {
-  const navigate = useNavigate(); //za navigaciju
-  return (
-    <div className="Navbar">
-      
-      <button className="goback" onClick={() => navigate(-1)}>Go Back</button> {/* Novo dugme Go Back*/}
-      <Link to="/" className="nav-link">
-        Calendar
-      </Link>
-      <Link to="/create-event" className="nav-link">
-        Create Event
-      </Link>
-      <Link to="/create-user" className="nav-link">
-        Create User
-      </Link>
-      <Link to="/event-list" className="nav-link">
-        Event List
-      </Link>
-      <Link to="/user-list" className="nav-link">
-        User List
-      </Link>
-    </div>
-  );
-};
-
-const CreateEventComponent = () => {
-  const [name, setName] = useState("");
-  const [date, setDate] = useState("");
-  const [description, setDescription] = useState("");
-  const navigate = useNavigate(); //za navigaciju
-  const handleCreateEvent = () => {
-    const newEvent = new Event(name, date, description);
-    const events = JSON.parse(localStorage.getItem("events")) || [];
-    events.push(newEvent);
-    localStorage.setItem("events", JSON.stringify(events));
-    console.log(newEvent.getEventDetails());
-  };
-  return (
-    <div className="create-event">
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Event Name"
-      />
-      <input
-        type="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        placeholder="Event Date"
-      />
-      <textarea
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Event Description"
-      ></textarea>
-      <button onClick={handleCreateEvent}>Create Event</button>
-      <button className="goback" onClick={() => navigate(-1)}>Go Back</button> {/* Novo dugme Go Back*/}
-    </div>
-  );
-};
-
- const CreateUserComponent = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const navigate = useNavigate(); //za navigaciju
-  const handleCreateUser = () => {
-    const newUser = new User(username, password, email);
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    users.push(newUser);
-    localStorage.setItem("users", JSON.stringify(users));
-
-    console.log(newUser.getUserInfo());
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <button onClick={handleCreateUser}>Create User</button>
-      <button className="goback" onClick={() => navigate(-1)}>Go Back</button> {/* Novo dugme Go Back*/}
-    </div>
-  );
-};
-
-const EventListComponent = () => {
-  const [events, setEvents] = useState([]);
-  const navigate = useNavigate(); //za navigaciju
-  useEffect(() => {
-
-    const storedEvents = JSON.parse(localStorage.getItem("events")) || [];
-    const events = storedEvents.map(
-      (event) => new Event(event.name, event.date, event.description)
-    );
-    setEvents(events);
-  }, []);
-
-  return (
-    <div>
-      {events.map((event, index) => (
-        <div key={index} className="event-list">
-          <p>{event.name}</p>
-          <p>{event.date}</p>
-          <p>{event.description}</p>
-        </div>
-      ))}
-      <button className="goback" onClick={() => navigate(-1)}>Go Back</button> {/* Novo dugme Go Back*/}
-    </div>
-  );
-};
-
-const UserListComponent = () => {
-  const [users, setUsers] = useState([]);
-  const navigate = useNavigate(); //za navigaciju
-  useEffect(() => {
-    const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
-    setUsers(
-      storedUsers.map(
-        (user) => new User(user.username, user.password, user.email)
-      )
-    );
-  }, []);
-
-  return (
-    <div>
-      {users.map((user, index) => (
-        <div key={index} className="user-list">
-          <h>{user.username}</h>
-          <p>{user.email}</p>
-        </div>
-      ))}
-      <button className="goback" onClick={() => navigate(-1)}>Go Back</button> {/* Novo dugme Go Back*/}
-    </div>
-  );
-};
-
- export const LoginPage = () => {
-  // Jos neki hooks-ovi za unosenje inputa i funkcije za brisanje teksta nakon kilka na dugme
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const logIn = () => {
-    if (email === "klijentske" && password === "klijentske") {
-      navigate("/profil");
-    } else {
-      alert("Inccorect email or password!");
-    }
-
-    setEmail("");
-    setPassword("");
-  };
-
-  return (
-    <div className="login-page">
-      {/* Prebacuje na pocetnu stranicu */}
-      <Link to="/" className="back-login">
-        GO BACK
-      </Link>
-      <h1 className="h1-login">LOGIN</h1>
-      <p className="p-login">Please enter your email and password!</p>
-      <div className="email-login">
-        {/* Unos emila */}
-        <input
-          type="text"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="Email"
-          className="email-login1"
-        />
-        {/* Unos sifre */}
-        <input
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder="Password"
-          className="email-login2"
-        />
-      </div>
-      <i className="link-login">Forgot password?</i>
-      {/* Klikom na dugme brise sve iz input-a */}
-      <button className="btn-login1" onClick={logIn}>
-        Log in
-      </button>
-      {/* Linkovi do sledecih aplikacija */}
-      <div className="app-buttons">
-        <a href="https://www.facebook.com" className="bx-link-login">
-          <i className="bx bxl-facebook"></i>
-        </a>
-        <a href="https://twitter.com" className="bx-link-login">
-          <i className="bx bxl-twitter"></i>
-        </a>
-        <a href="https://www.google.com" className="bx-link-login">
-          <i className="bx bxl-google"></i>
-        </a>
-      </div>
-      {/* Salje nas stranicu register */}
-      <p className="p2-login">
-        Don't have an account?{" "}
-        <Link to="/register" className="register-btn">
-          Sing up
-        </Link>
-      </p>
-    </div>
-  );
-};
-
-const ProfilPage = () => {
-  return (
-    <div className="profile">
-      <h1 className="nameSurname">Klijentske Klijentske</h1>
-      <div className="icon">
-        <i className="bx bxs-face-mask"></i>
-      </div>
-      <div className="info">
-        <div className="info1">
-          <i className="bx bxs-face-mask"></i>
-          <p className="info1-text">Name: Klijentske</p>
-        </div>
-        <div className="info1">
-          <i className="bx bxs-balloon"></i>
-          <p className="info1-text">Surname: Klijentske</p>
-        </div>
-        <div className="info1">
-          <i className="bx bx-money-withdraw"></i>
-          <p className="info1-text">Job: Student</p>
-        </div>
-        <div className="info1">
-          <i className="bx bx-home"></i>
-          <p className="info1-text">Addres: Jove Ilica</p>
-        </div>
-        <div className="info1">
-          <i className="bx bxs-mobile"></i>
-          <p className="info1-text">Phone: +381 64 </p>
-        </div>
-      </div>
-
-      <Link to="/login" className="goback">
-        Previous page
-      </Link>
-    </div>
-  );
-};
-
-const RegisterPage = () => {
-  // Postavljanje hook-ova za unos u  inpute
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [country, setCountry] = useState("");
-  const [addres, setAddres] = useState("");
-  const [phone, setPhone] = useState("");
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handlePassword2Change = (e) => {
-    setPassword2(e.target.value);
-  };
-
-  const handleCountrylChange = (e) => {
-    setCountry(e.target.value);
-  };
-
-  const handleAddresChange = (e) => {
-    setAddres(e.target.value);
-  };
-
-  const handlePhoneChange = (e) => {
-    setPhone(e.target.value);
-  };
-
-  // Funkcija vraca vrednosti na ""
-  const register = () => {
-    setAddres("");
-    setCountry("");
-    setPassword("");
-    setPassword2("");
-    setEmail("");
-    setPhone("");
-  };
-
-  return (
-    // Vraca nas na login stranicu
-    <div className="login-page">
-      <Link to="/login" className="back-login">
-        GO BACK
-      </Link>
-
-      <h1 className="h1-register">REGISTER</h1>
-      <p className="p-register">It’s quick and easy.</p>
-      {/* inputi */}
-      <div className="email-register">
-        <input
-          type="text"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="Email"
-          className="email-register1"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder="Password"
-          className="email-register1"
-        />
-        <input
-          type="password"
-          value={password2}
-          onChange={handlePassword2Change}
-          placeholder="Repeat password"
-          className="email-register1"
-        />
-        <input
-          type="text"
-          value={country}
-          onChange={handleCountrylChange}
-          placeholder="Country, City"
-          className="email-register1"
-        />
-        <input
-          type="text"
-          value={addres}
-          onChange={handleAddresChange}
-          placeholder="Addres"
-          className="email-register1"
-        />
-        <input
-          type="number"
-          value={phone}
-          onChange={handlePhoneChange}
-          placeholder="Phone"
-          className="email-register1"
-        />
-      </div>
-      <div>
-        {/* Klikom na register otvara se popup sa obavestenjem */}
-        <Popup
-          trigger={<button className="btn-register">REGISTER</button>}
-          modal
-        >
-          {(close) => (
-            <div className="popup-content">
-              <h1 className="popup-h1">Successfully registered!</h1>
-              <p className="popup-p">Thank you for your trust!</p>
-              <button
-                onClick={() => {
-                  register();
-                  close();
-                }}
-                className="btn-back"
-              >
-                Close
-              </button>
-            </div>
-          )}
-        </Popup>
-      </div>
-    </div>
-  );
-};
-
-const SuggestionPage = () => {
-  // Za textarea
-  const [myVariable, setMyVariable] = useState("");
-
-  // Funkcija koja ažurira state
-  const handleChange = (event) => {
-    setMyVariable(event.target.value);
-  };
-
-  // prazni myVariable
-  const handleSend = () => {
-    setMyVariable("");
-  };
-
-  return (
-    <div className="suggestion-div">
-      <h1 className="suggestion-h1">Your suggestion:</h1>
-      <div className="suggestion-text">
-        <textarea
-          value={myVariable}
-          onChange={handleChange}
-          rows="4"
-          cols="50"
-          className="suggestion-textarea"
-        />
-      </div>
-      {/* Jos jedan popup */}
-      <div className="suggestion-btns">
-        <Popup
-          trigger={<button className="suggestion-btn">Send!</button>}
-          modal
-          onClose={handleSend}
-        >
-          {(close) => (
-            <div className="popup-content">
-              <p className="popup-p">Thank you on your suggestions!</p>
-              <button onClick={close} className="btn-back">
-                Close
-              </button>
-            </div>
-          )}
-        </Popup>
-        {/* Vraca nas pocetnu stranicu */}
-        <Link to="/" className="suggestion-btn">
-          Go back!
-        </Link>
-      </div>
-    </div>
-  );
-};
-
 // Metso gde se nalaze sve stranice
 const App = () => {
   return (
@@ -759,12 +316,9 @@ const App = () => {
   );
 };
 
-
 export const sortByDate = (events) => {
   return events.sort((a, b) => new Date(a.date) - new Date(b.date));
 };
-
-
 
 export default App;
 //export { LoginPage };
