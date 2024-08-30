682ccdddc5f572559f9ccd03ccd131a7238031d2
MihailoRadulovic <161826642+MihailoRadulovic@users.noreply.github.com>
2024-08-30 10:10:55 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext' // ili 'es6'
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+  },
+});
